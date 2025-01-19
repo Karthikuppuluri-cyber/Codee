e95b2c15c31189310649f93fdc476b4c4d289e74
Karthikuppuluri-cyber <karthik@cognatrix.com>
2025-01-19 18:28:10 +0530

    Updated Text

--- data/Lookup.jsx
+++ data/Lookup.jsx
@@ -4,10 +4,10 @@ export default {
   SUGGSTIONS: ['Create ToDo App in React', 'Create Budget Track App', 'Create Gym Managment Portal Dashboard', 'Create Quizz App On History', 'Create Login Signup Screen'],
   HERO_HEADING: 'What do you want to build?',
   HERO_DESC: 'Prompt, run, edit, and deploy full-stack web apps.',
-  INPUT_PLACEHOLDER: 'What you want to build?',
-  SIGNIN_HEADING: 'Continue With Bolt.New 2.0',
-  SIGNIN_SUBHEADING: 'To use Bolt you must log into an existing account or create one.',
-  SIGNIn_AGREEMENT_TEXT: 'By using Bolt, you agree to the collection of usage data for analytics.',
+  INPUT_PLACEHOLDER: 'Build anything with Codiee',
+  SIGNIN_HEADING: 'Continue With Codiee',
+  SIGNIN_SUBHEADING: 'To use Codiee you must log into an existing account or create one.',
+  SIGNIn_AGREEMENT_TEXT: 'By using Codiee, you agree to the collection of usage data for analytics.',
 
 
   DEFAULT_FILE: {
